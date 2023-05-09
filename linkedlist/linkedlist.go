ffd47490f01a4694b7a9333f392e045b113b48eb
Promise Femi <promisefemi90@gmail.com>
2023-05-09 13:57:53 +0100

    Delete first and last node

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -2,46 +2,102 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
-type link struct {
+type linkedList struct {
+	// value can be whatever datatype fits your particular scenario
 	value string
-	next  *link
+	next  *linkedList
 }
 
-func (l *link) insert(text string) {
-	temp := &link{}
-	temp.value = text
-	temp.next = nil
-
-	if l == nil {
-		l = temp
+func (node *linkedList) insert(text string) {
+	// first we create a temporary value for our new node
+	tempNode := &linkedList{value: text, next: nil}
+	// remember that the next pointer in the last node is always nil until it points to another node
+	if node == nil {
+		// if this is a head node and it is nill, make the current tempNode the head
+		node = tempNode
 	} else {
-		currentNode := l
+		currentNode := node
+		// go through every node until we get to the last node,
 		for currentNode.next != nil {
+			// check if it is the last node, if not move to the next
 			currentNode = currentNode.next
 		}
-		currentNode.next = temp
+		// when we get to the last node, place the new node we are creating
+		currentNode.next = tempNode
 	}
 }
-func (l *link) print() {
-	var currentNode = &link{}
-	currentNode = l.next
+
+func (node *linkedList) print() {
+	// start from the first node
+	currentNode := node
+	// loop through each node, making sure it is not nil
 	for currentNode != nil {
-		fmt.Printf("%s ->", currentNode.value)
+		//print the value out
+		fmt.Printf("%s -> ", currentNode.value)
+		//move to the next node
 		currentNode = currentNode.next
 	}
+	fmt.Println("")
+}
+
+func (node *linkedList) deleteFirstNode() *linkedList {
+	node = node.next
+	return node
+}
+
+func (node *linkedList) deleteLastNode() {
+	currentNode := node
+	nextNode := node.next
+	for nextNode.next != nil {
+		nextNode = nextNode.next
+		currentNode = currentNode.next
+	}
+	currentNode.next = nil
+	/*
+		A different way of solving the problem
+		p := node
+		for p.next.next != nil {
+			p = p.next
+		}
+		p.next = nil*/
 }
 
 func main() {
-	head := new(link)
+	list := new(linkedList)
 
-	head.insert("Monday")
+	for {
+		var selected string
+		fmt.Println("(1) enter a new item")
+		fmt.Println("(2) print list")
+		fmt.Println("(3) delete from head/beginning")
+		fmt.Println("(4) delete from last")
+		_, err := fmt.Scanln(&selected)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	head.insert("Wednesday")
-	head.insert("Tuesday")
-	head.insert("Thursday")
-	head.insert("Friday")
+		switch selected {
+		case "1":
+			fmt.Println("Enter new value")
+			var item string
+			_, err := fmt.Scanln(&item)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			list.insert(item)
+		case "2":
+			list.print()
+		case "3":
+			list = list.deleteFirstNode()
+		case "4":
+			list.deleteLastNode()
+		default:
+			os.Exit(0)
 
-	head.print()
+		}
+	}
 }
